1724d93fd53d4282ffb8d9d8a2936a292d77c472
hifromkevin <lam.kevin86@gmail.com>
2018-02-07 19:11:45 -0800

    Displays index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,29 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var header = helpers.headers;
+var serveAss = helpers.serveAssets;
+var fs = require('fs');
+var index = '';
+var indexMaker = fs.readFile('./public/index.html', function (err, html) {
+  if (err) {
+    throw err;
+  } 
+  index += html;
+});
+//make indexloading which is in public folder. Same method as index 
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode = 200;
+  var converse = {};
+  console.log('dirname', __dirname);
+  converse.results = [];
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(statusCode, header);
+    
+    res.end(index);
+    // res.end(archive.paths.list);
+  }
 };
